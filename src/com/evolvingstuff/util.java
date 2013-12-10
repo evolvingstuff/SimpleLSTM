89ecec5fd64c564812a9b98e63690f1c77c9b0c6
cy <cy.rilauburtin@gmail.com>
2013-12-11 00:49:49 +0100

    added some classic tests

--- src/com/evolvingstuff/util.java
+++ src/com/evolvingstuff/util.java
@@ -6,8 +6,7 @@ public class util
 		int result = -1;
 		double max = Double.NEGATIVE_INFINITY;
 		for (int i = 0; i < vec.length; i++) {
-			if (vec[i] > max)
-			{
+			if (vec[i] > max){
 				max = vec[i];
 				result = i;
 			}
@@ -19,12 +18,40 @@ public class util
 		int result = -1;
 		double min = Double.POSITIVE_INFINITY;
 		for (int i = 0; i < vec.length; i++) {
-			if (vec[i] < min)
-			{
+			if (vec[i] < min){
 				min = vec[i];
 				result = i;
 			}
 		}
 		return result;
+	}
+	
+	public static double getSquaredError(double[] vector1, double[] vector2) {
+        double squaredError = 0;
+        for (int i = 0; i < vector1.length; i++) {
+            squaredError += (vector1[i] - vector2[i]) * (vector1[i] - vector2[i]);
+        }
+        return squaredError;
+    }
+	
+	public static double[] sin(double... v){
+		double[] r = new double[v.length];
+		for (int i=0; i<v.length; i++)
+			r[i] = Math.sin(v[i]);
+		return r;
+	}
+	
+	public static double[] seq(double ini, int n, double inc){
+		double[] r = new double[n];
+		for (int i=0; i<n; i++)
+			r[i] = ini+=inc;
+		return r;
+	}
+	
+	public static String join(double[] a){
+	    String r=a[0]+"";
+	    for (int i=1; i<a.length; i++)
+	        r+="\t"+a[i];
+	    return r;
 	}
 }
